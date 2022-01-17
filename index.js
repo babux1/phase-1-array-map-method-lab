443ab3aa7caaae3ad8960a0d1937bdb11585f1d2
Batmunkh Namshir <namshir17@gmail.com>
2022-01-17 15:25:35 -0800

    commit tree 1dea4a27e12ec310db95643e344be3be3d2e8103

--- index.js
+++ index.js
@@ -12,5 +12,15 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((elements) => {
+    const tokens = elements.split(" ");
+    const capitalizedTokens = tokens.map(
+      (token) => token.charAt(0).toUpperCase() + token.slice(1)
+    );
+    const response = capitalizedTokens.join(" ");
+    return response;
+  });
+};
+
+
+console.log(titleCased())
